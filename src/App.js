c7b0c24f7b472699b8435e3f128508740c772fe1
Diogo Sousa <diogosousainf@gmail.com>
2024-02-04 22:32:48 +0000

    firstPart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React,{useState, useEffect} from 'react';
 import axios from 'axios';
+
+import Navbar from './components/Navbar';
 import Coins from './components/Coins';
+
+
 
 function App() {
 
 
   const [coins, setCoins] = useState([]);
 
-  const url= 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en';
+  const url= 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&locale=en';
 
    useEffect(() => {
     axios.get(url).then((response) => {
@@ -21,6 +25,7 @@ function App() {
 
   return (
     <>
+    <Navbar />  
     <Coins coins={coins} />
 
     </>
--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import CoinItem from './CoinItem';
 
+import './Coins.css';
+
 const Coins = (props) => {
     return (
         <div className='container'>
@@ -16,22 +18,10 @@ const Coins = (props) => {
 
                 {props.coins.map((coin) => {
                     return (
-                        <div className='coin-row' key={coin.id}>
-                            <p>{coin.market_cap_rank}</p>
-                            <div className='img-symbol'>
-                                <img src={coin.image} alt='crypto' />
-                                <p>{coin.symbol}</p>
-                            </div>
-                            <p>${coin.current_price}</p>
-                            <p>${coin.price_change_percentage_24h}</p>
-                            <p className='hide-mobile'>${coin.total_volume}</p>
-                            <p className='hide-mobile'>${coin.market_cap}</p>
-                        </div>
+                        <CoinItem coins={coin} key={coin.id} />                      
                     )
                 })}
-
-            </div>
-           
+            </div>  
         </div>
     )
     }
